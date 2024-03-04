4eee45fc79ec2d9c0381840f4f02fd87ecb95164
gustavo.costa <gustavoalberttodev@gmail.com>
2024-03-04 16:25:03 -0300

    Atualização dos seeds

--- rest_api/db/seeds.rb
+++ rest_api/db/seeds.rb
@@ -1,3 +1,3 @@
 Insured.create!(name: 'John Doe', cpf: '123.456.789-00')
 Vehicle.create!(plate: 'ABC-1234', year: 2022, model: 'Fusca', brand: 'Volkswagen')
-Policy.create!(issue_date: '2022-02-22', coverage_end: '2023-02-22', insured_id: 1, vehicle_id: 1)
+Policy.create!(issue_date: '2022-02-22', coverage_end: 1.year.from_now, insured_id: 1, vehicle_id: 1)

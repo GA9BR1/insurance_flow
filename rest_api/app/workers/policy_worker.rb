7a86ab393b6af18f129337a0c7fd868626df0869
gustavo.costa <gustavoalberttodev@gmail.com>
2024-04-11 11:47:55 -0300

    melhorias de código

--- rest_api/app/workers/policy_worker.rb
+++ rest_api/app/workers/policy_worker.rb
@@ -11,42 +11,11 @@ class PolicyWorker
     puts parsed_json
     ActiveRecord::Base.connection_pool.with_connection do
       ActiveRecord::Base.transaction do
-        insured = Insured.find_or_create_by!(parsed_json["insured"])
-        vehicle = Vehicle.create!(parsed_json["vehicle"])
-        policy = Policy.create!(issue_date: parsed_json["issue_date"], coverage_end: parsed_json["coverage_end"],
-                                prize_value: parsed_json["prize_value"], insured:, vehicle:)
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-        response = Stripe::Price.create({
-          currency: 'brl',
-          unit_amount: parsed_json['prize_value'].to_i * 100,
-          product_data: { name: 'Seguro' },
-        })
-        response_payment = Stripe::PaymentLink.create({
-          line_items: [
-            {
-              price: response.id,
-              quantity: 1,
-            },
-          ],
-          metadata: {
-            policy_id: policy.id
-          }
-        })
-        policy.update!(payment_link: response_payment.url)
-        response = Net::HTTP.post(URI('http://web_app:3000/send_to_websockets'),
-        {"type" => "policy_created", "policy" => PolicySerializer.serialize(policy)}.to_json,
-          'Content-Type' => 'application/json',
-          'Authorization' => "Bearer #{JWT.encode({id: SecureRandom.uuid}, ENV['JWT_SECRET'], 'HS256')}"
-        )
+        policy = create_policy(parsed_json)
+        policy.update!(payment_link: generate_payment_link(policy, parsed_json).url)
+        send_policy_creation_websockets_message(policy)
       rescue StandardError => e
-        response = Net::HTTP.post(URI('http://web_app:3000/send_to_websockets'),
-        {"type" => "policy_creation_error", "message" => e}.to_json,
-
-          'Content-Type' => 'application/json',
-          'Authorization' => "Bearer #{JWT.encode({id: SecureRandom.uuid}, ENV['JWT_SECRET'], 'HS256')}"
-        )
-        puts "Error occurred during transaction: #{e.message}"
-        Sneakers.logger.error "Error occurred during transaction: #{e.message}"
+        send_policy_creation_error_websockets_message(e)
         Stripe::PaymentLink.retrieve(response_payment.id)
         handle_error(msg, e)
       end
@@ -55,6 +24,50 @@ class PolicyWorker
   end
 
   private
+
+  def create_policy(parsed_json)
+    insured = Insured.find_or_create_by!(parsed_json["insured"])
+    vehicle = Vehicle.create!(parsed_json["vehicle"])
+    Policy.create!(issue_date: parsed_json["issue_date"], coverage_end: parsed_json["coverage_end"],
+                            prize_value: parsed_json["prize_value"], insured:, vehicle:)
+  end
+
+  def send_policy_creation_websockets_message(policy)
+    Net::HTTP.post(URI('http://web_app:3000/send_to_websockets'),
+      {"type" => "policy_created", "policy" => PolicySerializer.serialize(policy)}.to_json,
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{JWT.encode({id: SecureRandom.uuid}, ENV['JWT_SECRET'], 'HS256')}"
+      )
+  end
+
+  def send_policy_creation_error_websockets_message(error)
+    response = Net::HTTP.post(URI('http://web_app:3000/send_to_websockets'),
+        {"type" => "policy_creation_error", "message" => error}.to_json,
+
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{JWT.encode({id: SecureRandom.uuid}, ENV['JWT_SECRET'], 'HS256')}"
+        )
+  end
+
+  def generate_payment_link(policy, parsed_json)
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    response = Stripe::Price.create({
+      currency: 'brl',
+      unit_amount: parsed_json['prize_value'].to_i * 100,
+      product_data: { name: 'Seguro' },
+    })
+    Stripe::PaymentLink.create({
+      line_items: [
+        {
+          price: response.id,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        policy_id: policy.id
+      }
+    })
+  end
 
   def handle_error(msg, error)
     error_message = {
